601937c70fd683e9935a8221c163777c822cf138
scottgeraedts <scott.geraedts@gmail.com>
2017-04-06 11:20:05 -0400

    working version

--- shrinker.h
+++ shrinker.h
@@ -38,7 +38,7 @@ void TorusSolver<ART>::makeShrinker(int nx){
 			temptrips.push_back( Eigen::Triplet< complex<double> >( col,index,polar(1.*sign,phase*2*M_PI/(1.*this->Ne)*nx ) ) );
 			found_states.push_back(index);
 //			cout<<(bitset<12>)this->states[index]<<" "<<phase*nx%this->Ne<<" "<<sign<<endl;
-			temp=cycle_M(temp,this->Ne,this->NPhi,invNu,sign);
+			temp=cycle_M(temp,this->NPhi,invNu,sign);
 			if(temp==this->states[i]) break;
 			it=lower_bound(this->states.begin(),this->states.end(),temp);
 			phase++;
